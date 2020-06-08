768857e84fea1a7afd6dc6fa2131d5d76a2a204e
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-06-08 19:15:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,7 @@ class School
   end
   
   def grade(number)
+<<<<<<< HEAD
     roster[number]
   end
   
@@ -23,4 +24,10 @@ class School
     new_hash
   end
     
+=======
+    
+  end
+  
+  
+>>>>>>> 8397e65852e243d9a9664618f92790d90bbb34bf
 end
